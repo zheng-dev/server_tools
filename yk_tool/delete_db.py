850a6fd2490c0d5b7e39409e8a7ec668dc9d3330
zheng-dev <zheng6655@163.com>
2024-07-08 18:11:26 +0800

    生成配置

--- yk_tool/delete_db.py
+++ yk_tool/delete_db.py
@@ -2,25 +2,54 @@
 # coding=utf-8
 #py3.11
 # 运行方式：python delete_db.py ../
-import os, sys, shutil
+import os,shutil
 
+import sys
+import time
 
+_DB_CFG_="db_path.cfg"
 def main():
-   # 参数是db文件夹所在的路径
-   try:
-    args = sys.argv[1]
-    print(("=start args=:"+args))
-    del_tab_bak_dir(args)
-    print("=success====")
-   except  FileNotFoundError as e:
-      print("err:%s %s"%(e.strerror,e.filename))
-      return -1
+   """安装或执行清理"""
+   if len(sys.argv)>1:
+       install_timer()
+   else:
+       current_path = os.path.dirname(__file__)
+       r=os.chdir(current_path)
+       now=time.localtime(time.time())
+       sys.stdout = open('output{0}{1}.txt'.format(now.tm_mon,now.tm_mday), 'w+')
+    #    with open("a.txt",'a+') as f:  
+    #        size=f.seek(0,os.SEEK_END) 
+    #        if size>2024:#2K
+    #             f.seek(0)                                  
+    #        f.write("done={0}=={1}={2},size={3}\n".format(time.time(),current_path,r,size))
+       del_cfg_tab()#定时清理中
+   
 
+def del_cfg_tab():
+    """从配置中读取要清的db所在目录,遍历清除增量目录"""
+    try:
+        with open(_DB_CFG_,'r',encoding='utf-8') as f:
+            while f:
+                line=f.readline()
+                if line=="":
+                    break
+                #去掉空行
+                line=line.strip()
+                if line=="" or line[0]=="#":
+                    continue
+                del_tab_bak_dir(line)
+    except FileNotFoundError as e:
+        print("err:{0},{1}".format(e.strerror,e.filename))
+    return 0
 
 def del_tab_bak_dir(dbPath):
     """删除表目录下的备份文件夹"""
     # 强行到指定的db目录去操作
-    os.chdir(dbPath + "/db")
+    try:
+        os.chdir(dbPath + "/db")
+    except OSError as e:
+        print("path_no_db-->{0}".format(e.filename))
+        return -1    
     for db in os.listdir("."):
         os.chdir(db)# 进库
         for table in os.listdir("."):
@@ -37,10 +66,30 @@ def del_tab_bak_dir(dbPath):
                     except OSError:
                         print(("rm err:" + dir))
                         continue
-            # print(table, dataDirL)
             os.chdir("../")
         os.chdir("../")
     return
 
+def install_timer():
+    """根据系统安装定时器安装"""
+    if sys.platform=="win32":
+        #currentPath = os.getcwd().replace('\\','/')    # 获取当前路径
+        #SCHTASKS /DELETE /TN "yk_db_bak_del"
+        r=os.system("schtasks /create /sc minute /mo 1 /tn \"yk_db_bak_del\" /tr pyw\" {0}\"".format(__file__))
+        if r==1:
+            print("定时器失败,确认是否管理员权限:{0}".format(r))
+        else:            
+            #初始配置
+            with open(_DB_CFG_,'w+',encoding='utf-8') as f:
+                if f:
+                    f.writelines(["#配置注释可以是#开关的行\n","#本配置必需是utf8文件\n","#db所在路径的配置,可以是多个项目如\n","#F:\\snk_work\\server_code\\alpha\\game_alpha\n"])
+            print("win timer ok:{0},init cfg={1}".format(r,_DB_CFG_))
+
+       
+        pass
+    else:
+        print("linux timer:")
+        pass
+    return 1
 
 main()

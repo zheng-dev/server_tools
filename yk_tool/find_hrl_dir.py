1802b2e29d726e09ca6fc0903de7eae8fcb6a8a0
zheng-dev <zheng6655@163.com>
2024-07-09 23:43:56 +0800

    fix 代码格式

--- yk_tool/find_hrl_dir.py
+++ yk_tool/find_hrl_dir.py
@@ -7,43 +7,44 @@ import os,shutil
 import sys
 
 
-_G_find_ret=[]#遍历目录时存path用
+_GFindRet=[]#递归遍历目录时存path用
 
 def main():
-   """安装或执行清理"""
    if len(sys.argv)>1:
        dir=sys.argv[1]
    else:
        dir="."
 
-   find_dir("",dir)
-   global _G_find_ret
-   with open("hrl.txt",'w+') as f:
-       for i in _G_find_ret:
-           f.write("<sourceFolder url=\"file://$MODULE_DIR$/plugin{0}\" type=\"erlang-include\" />\n".format(i[1:]))
-           pass
-       pass
+   _find_dir("",dir)
+   _after_find()
    return
 
-
-def find_dir(Prent:str,path:str)->None:
+#查找出头目录到全局变量中
+def _find_dir(Prent:str,path:str)->None:
     os.chdir(path)
     isHrlDir=False
     for f in os.listdir("."):
         if os.path.isdir(f):
-            find_dir(Prent+path+"/",f)
+            _find_dir(Prent+path+"/",f)
         elif isHrlDir:
             continue
         else:
            if (os.path.splitext(f)[-1])==".hrl":
                 isHrlDir=True
-                global _G_find_ret
-                _G_find_ret.append("{0}{1}".format(Prent,path))
+                global _GFindRet
+                _GFindRet.append("{0}{1}".format(Prent,path))
         pass
     os.chdir("../")
     return
-
-
+#使用全局变量的目录结果生成文件
+def _after_find():
+    global _GFindRet
+    with open("hrl.txt",'w+') as f:
+        for i in _GFindRet:
+            f.write("<sourceFolder url=\"file://$MODULE_DIR$/plugin{0}\" type=\"erlang-include\" />\n".format(i[1:]))
+            pass
+        pass
+    return
 
 
 main()

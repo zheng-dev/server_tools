2dd4563559cda61d16ab1e3f040fa715b64fbe26
zheng-dev <zheng6655@163.com>
2024-07-24 16:25:52 +0800

    bak

--- yk_tool/find_hrl_dir.py
+++ yk_tool/find_hrl_dir.py
@@ -7,48 +7,59 @@ import os,shutil
 import sys
 
 
-_GFindRet=[]#递归遍历目录时存path用
-__GAME='game.iml'#文件
+
+GAME='game.iml'#文件
 
 def main():
    if len(sys.argv)>1:
        dir=sys.argv[1]
    else:
        dir="."
-    
-   #检查目录   
-   check_pro_dir()
-   __find_dir("",dir)
-   __after_find()
+   FindHrlDir.go(dir)
+
    return
 
-def check_pro_dir()->None:
-    if not os.path.isfile(__GAME):
-        raise "pro_dir_err"
-    return
+class FindHrlDir:
+    _GFindRet=[]#递归遍历目录时存path用
+    def go(dir):
+        a=FindHrlDir()
+        os.chdir('plugin')
+        a._find_dir("",dir)
+        os.chdir('../')
+        a._after_find()
 
-#查找出头目录到全局变量中
-def __find_dir(Prent:str,path:str)->None:
-    os.chdir(path)
-    isHrlDir=False
-    for f in os.listdir("."):
-        if os.path.isdir(f):
-            __find_dir(Prent+path+"/",f)
-        elif isHrlDir:
-            continue
-        else:
-           if (os.path.splitext(f)[-1])==".hrl":
-                isHrlDir=True
-                global _GFindRet
-                _GFindRet.append("{0}{1}".format(Prent,path))
+    def __init__(self) -> None:
+        #检查目录是否是idea项目
+        if not os.path.isfile(GAME):
+            raise "pro_dir_err"
         pass
-    if Prent!="":
-        os.chdir("../")
-    return
-#使用全局变量的目录结果生成文件
-def __after_find():
-    global _GFindRet
+    #查找出头目录到全局变量中
+    def _find_dir(self,Prent:str,path:str)->None:
+        os.chdir(path)
+        isHrlDir=False
+        for f in os.listdir("."):
+            if os.path.isdir(f):
+                self._find_dir(Prent+path+"/",f)
+            elif isHrlDir:
+                continue
+            else:
+                if (os.path.splitext(f)[-1])==".hrl":
+                        isHrlDir=True
+                        self._GFindRet.append("{0}{1}".format(Prent,path))
+                pass
+        if Prent!="":
+            os.chdir("../")
+        return
+    #使用全局变量的目录结果生成文件
+    def _after_find(self):
+        with open("hrl.txt",mode='w+') as f:
+            for i in self._GFindRet:
+                f.write("<sourceFolder url=\"file://$MODULE_DIR$/plugin{0}\" type=\"erlang-include\" />\n".format(i[1:]))
+                pass
+            pass
+        return
 
+def __test_xml():
     # import xml.dom.minidom 
     # tree=xml.dom.minidom.parse(__GAME)
     # collection=tree.documentElement
@@ -62,7 +73,7 @@ def __after_find():
     #     print("t:{0}".format(type.childNodes[0].data))    
 
     import xml.etree.ElementTree as et
-    tree=et.parse(__GAME)
+    tree=et.parse(GAME)
     p_root=tree.getroot()
     # for m in p_root.findall('movie'):
     #     t=m.find('type').text
@@ -74,14 +85,9 @@ def __after_find():
     test=et.SubElement(m,"test")
     test.text='33'
     test.attrib['bb']='c'
-    tree.write('aa.iml',"UTF-8")
-
-    with open("hrl.txt",mode='w+') as f:
-        for i in _GFindRet:
-            f.write("<sourceFolder url=\"file://$MODULE_DIR$/plugin{0}\" type=\"erlang-include\" />\n".format(i[1:]))
-            pass
-        pass
-    return
+    tree.write('aa.iml',"UTF-8",True,None,'xml')
 
 if __name__=='__main__':
     main()
+    #__test_xml()
+    pass

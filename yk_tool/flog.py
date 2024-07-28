799b3117cdb1166e60ee849c6968fbddf199ba89
zheng-dev <zheng6655@163.com>
2024-07-28 15:38:28 +0800

    查找

--- yk_tool/flog.py
+++ yk_tool/flog.py
@@ -2,13 +2,8 @@
 # coding=utf-8
 #py3.11
 # 运行方式：python flog.py 
-import os,shutil
-
-import sys,glob,signal
-
-def sig_hand(a,b):
-    print("ctr+c exit")
-    sys.exit(0)
+#import os,shutil
+import sys
 
 def main():
    if len(sys.argv)>1:
@@ -16,7 +11,7 @@ def main():
            d_line()
            pass
        else:
-           find()
+           Find().find()
    else:
        print('''
 从匹配的文件中查找内容，显示所在文件名和行数
@@ -26,40 +21,80 @@ py -m flog -line xxxx.log 3
              ''')
    return
 #检查
-def find():
-   files=glob.glob(sys.argv[1])   
-   startStr= sys.argv[2]
-   endStr=sys.argv[3]
-   print(sys.argv[1],startStr,endStr,files)
+class Find:
+    retList=[]
+    allPage=0#总页数
+    currPage=1#当前页码
+    rowNum4Page=20#每页显示条数
+    page=[]#当前页内容
+    def find(self):
+        import glob
+        files=glob.glob(sys.argv[1])   
+        startStr= sys.argv[2]
+        endStr=sys.argv[3]
+        print(sys.argv[1],startStr,endStr,files)
+        #整理出结果[(findStr，file,lineNum)]
+        for f in files:
+            with open(f,'r',-1,'utf8') as fPtr:
+                lineNum=0
+                while fPtr:
+                    line=fPtr.readline()
+                    lineNum+=1
+                    if line=="":
+                        break
+                    i=line.find(startStr)
+                    if i>-1:
+                        i2=line.find(endStr,i)
+                        if i2>-1:
+                            self.retList.append((line[i:i2+1],f,lineNum))
+                    pass
+        #显示
+        self.retList.sort() 
+        self.display() if self.calc_page() else print("no match")
 
-   disNum=0
-   for f in files:
-       with open(f,'r',-1,'utf8') as fPtr:
-           lineNum=0
-           while fPtr:
-            line=fPtr.readline()
-            lineNum+=1
-            if line=="":
-                break
-            i=line.find(startStr)
+    #计算页码       
+    def calc_page(self):
+        if len(self.retList)<1:
+            self.currPage=0
+            return False
+        else:
+            p=int(len(self.retList)/self.rowNum4Page)
+            self.allPage=p if len(self.retList) % self.rowNum4Page ==0 else p+1 
+            return True
+    #逐页显示
+    def display(self):
+        if self.currPage<=self.allPage:
+            self.page=self.retList[(self.currPage-1)*self.rowNum4Page:self.currPage*self.rowNum4Page]
+            i=1
+            for (findStr,file,lineNum) in self.page:
+                print(f"{i}:{findStr}-->{file}:{lineNum}")
+                pass
+        print(f"页码{self.currPage}/{self.allPage} 每页{self.rowNum4Page}条")
+        #用户操作
+        self.cmd()
 
-            if disNum==20:
-                while True:
-                    moreOrLine=input("more or line")
-                    try:
-                        LNum=int(moreOrLine.strip())
-                        if LNum<=lineNum and LNum>0:
-                            d_line_(f,LNum)
-                        else:
-                            break    
-                    except:
-                        break    
-                disNum=0
-            if i>-1:
-                disNum+=1
-                i2=line.find(endStr,i)
-                print("{0}==>{1}-->{2}".format(line[i:i2+1],f,lineNum))
-            pass
+    def cmd(self):
+        cmd=input("下一页(n);前一页(b);显示指定原内容(d 条目id)")
+        cmd2=cmd.strip()
+        if cmd2=='b' and self.currPage>2:
+            self.currPage-=1
+            self.display()
+        elif cmd2=='n' and self.currPage<self.allPage:
+            self.currPage+=1
+            self.display()
+        elif cmd2[:2]== 'd ':
+            try:
+                line=int(cmd2[2:])
+                if line>=1 and line<=self.rowNum4Page:
+                    (findStr,file,lineNum)=self.page[line-1]
+                    d_line_(file,lineNum)
+                    pass
+                else:
+                    raise("error")
+            except:
+                print(f"输入无效:{cmd2}")    
+        else:
+            print(f"输入无效:{cmd2}")   
 
 #显示行
 def d_line():
@@ -83,13 +118,10 @@ def d_line_(file:str,lineNumArg:int):
 
 def analyse():
     (lineNum,condLineNum,kvList)=clear_data()
-
     print("总条数{0};100ms的条数{1}".format(lineNum,condLineNum))
-    #print(kvList)  
-    #print('事件mf | 总次数 | 100m的次数 | 平均用时 | 最大用时 | 原日志')
     save_ret(kvList)
-    
     return
+##结果存入csv
 def save_ret(kvList:dict):
     import csv
     if {}!=dict:
@@ -99,7 +131,6 @@ def save_ret(kvList:dict):
             #writer.writerow(['事件mf | 总次数 | 100m的次数 | 平均用时 | 最大用时 | 原日志'])
             for key in kvList:
                 oldTimes,old100Times,oldAccMs,oldMaxMs,oldLine=kvList.get(key)
-                #事件mf,总次数（大于100MS)，总用时(大于100ms的)不用显示,平均用时，最大用时
                 writer.writerow([key,oldTimes,old100Times,round(oldAccMs/oldTimes),oldMaxMs,oldLine.strip()])  
                 #print("{0} | {1} | {5} | {2} | {3} | {4}".format(key,oldTimes,round(oldAccMs/oldTimes),oldMaxMs,oldLine.strip(),old100Times))    
     return        
@@ -134,9 +165,7 @@ def clear_data()->tuple[int, int, dict]:
 
                 oldRow=kvList.get(mfStr,0)
                 #统一上100ms的
-                reach100=0
-                if useMs>100:
-                    reach100=1
+                reach100=0 if useMs>100 else 1
 
                 if oldRow==0:
                     #(总次数,总用时ms,上100ms的总次数,单次最大用时ms,最大ms时的line)
@@ -163,101 +192,60 @@ def progress(max:int,currIndex:int):
 
 class progress:
     ##无总量的,每100数刷一下
-    def progress_no_sum(self,currIndex:int):
-        rate=10000
+    def progress_no_sum(self,currIndex:int,rate:int=10000):
         if (currIndex % rate)==0:
-            if ((currIndex)/rate %2)==0:
-                f='/'
-            else:
-                f='\\'    
+            f='/' if ((currIndex)/rate %2)==0 else '\\'    
             print("\r\033[1;32m curr:{0}  {1}  \033[m ".format(currIndex,f),end="")
 
     def end(self):
         del self        
     ##进度完成
     def __del__(self):
-    #     字色              背景              颜色
-    # ---------------------------------------
-    # 30                40              黑色
-    # 31                41              紅色
-    # 32                42              綠色
-    # 33                43              黃色
-    # 34                44              藍色
-    # 35                45              紫紅色
-    # 36                46              青藍色
-    # 37                47              白色
+    #前景：黑色30紅色31綠色32黃色33藍色34紫紅色35青藍色36白色37
+    #背景：在前景值 基础上+10
         print("\033[1;37m \n=done=")
 
 def test():
-    import time
-    
     print("\033[20A\033[?25l",end="")
     for i in range(60):
-        time.sleep(1)
+        s=get_single_char()
         print("\033[2J",end="")
-        print("\033[31m 红色{0}字 \033[m".format(i))
-##不用回车        
-import tkinter
-from tkinter import ttk
- 
- 
-def xFunc1(event):
-    print(f"事件触发键盘输入:{event.char},对应的ASCII码:{event.keycode}")
- 
-def win():
-    win = tkinter.Tk()
-    win.title("Kahn Software v1")    # #窗口标题
-    win.geometry("600x500+200+20")   # #窗口位置500后面是字母x
-    '''
-    响应所有事件(键盘)
-    <Key>   所有键盘按键会触发
-    '''
-    xLabel = tkinter.Label(win, text="KAHN Hello world")
-    xLabel.focus_set()
-    xLabel.pack()
-    xLabel.bind("<Key>", xFunc1)
-    
-    win.mainloop()   # #窗口持久化
+        print("\033[31m 红色{0}{1}字 \033[m".format(i,s))
 
-#win
-def cmd():
-    import msvcrt  
-    print("按任意键继续...")  
-    while True:  
-        if msvcrt.kbhit():  # 检查是否有按键被按下  
-            char = msvcrt.getch().decode()  # 读取按键并解码为字符  
-            if char == '\r':  # 处理回车键  
-                break  
-            print("你按了:", char)
-def shell():
-        
-    # 使用函数  
-    char = get_single_char()  
-    print(f"You pressed: {char}")
-  
-def get_single_char():  
-        import sys  
-        import tty  , termios  
-        # 保存旧的终端设置  
-        fd = sys.stdin.fileno()  
-        old_settings = termios.tcgetattr(fd)  
-    
-        # 设置新终端设置：无回显，非阻塞  
-        try:  
-            tty.setraw(sys.stdin.fileno())  
-            ch = sys.stdin.read(1)  
-        finally:  
-            # 恢复旧的终端设置  
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)  
-    
-        return ch  
+##不用回车的单次输入
+def get_single_char():
+    if sys.platform=='win32':
+        return w__get_single_char()
+    else:
+        return l_get_single_char()  
+def w__get_single_char():
+    import msvcrt
+    return msvcrt.getch().decode()
+def l_get_single_char():  
+    import sys,tty,termios  
+    fd = sys.stdin.fileno()  
+    old_settings = termios.tcgetattr(fd)  
+    # 设置新终端设置：无回显，非阻塞  
+    try:  
+        tty.setraw(sys.stdin.fileno())  
+        ch = sys.stdin.read(1)  
+    finally:  
+        # 恢复旧的终端设置  
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)  
+    return ch  
 
-
+##捕获信号
+def sig_hand():
+    import signal
+    def a1(a,b):
+            print("ctr+c exit")
+            sys.exit(0) 
+    signal.signal(signal.SIGINT,a1)
+    
 ##
 if __name__=='__main__':
-    signal.signal(signal.SIGINT,sig_hand)
+    sig_hand()
     #main()
     #analyse()
-    #test()
-    cmd()
+    test()
     pass

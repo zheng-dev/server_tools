8a4cb50f4565d0532f8fd1d2c26209727b48518a
zheng-dev <zheng6655@163.com>
2024-07-28 14:02:23 +0800

    支持gui

--- yk_tool/find_hrl_dir.py
+++ yk_tool/find_hrl_dir.py
@@ -6,8 +6,6 @@ import os,shutil
 
 import sys
 
-
-
 GAME='game.iml'#文件
 
 def main():
@@ -60,18 +58,6 @@ class FindHrlDir:
         return
 
 def __test_xml():
-    # import xml.dom.minidom 
-    # tree=xml.dom.minidom.parse(__GAME)
-    # collection=tree.documentElement
-    # if collection.hasAttribute("shelf"):
-    #     print("root {0}".format(collection.getAttribute("shelf")))
-    # ms=collection.getElementsByTagName("movie")
-    # for m in ms:
-    #     if m.hasAttribute('title'):
-    #         print("has:{0}".format(m.getAttribute('title')))
-    #     type=m.getElementsByTagName('type')[0]
-    #     print("t:{0}".format(type.childNodes[0].data))    
-
     import xml.etree.ElementTree as et
     tree=et.parse(GAME)
     p_root=tree.getroot()
@@ -87,7 +73,46 @@ def __test_xml():
     test.attrib['bb']='c'
     tree.write('aa.iml',"UTF-8",True,None,'xml')
 
+def win():
+    import tkinter,tkinter.messagebox
+    win = tkinter.Tk()
+    win.title("iml-hrl目录增加")    # #窗口标题
+    win.geometry("300x90+900+110")   # #窗口位置500后面是字母x
+
+    l1=tkinter.Label(win,text='项目路径')
+    l1.pack()
+    dir=tkinter.Entry(win,width=20)
+    
+    dir.insert(0,os.path.dirname(__file__))
+    dir.pack()
+    dir.focus_set()
+    def goBack():
+        pathStr=dir.get()
+        print(f"=={pathStr}")
+        try:
+            os.chdir(pathStr)
+            __test_xml()
+            
+            tkinter.messagebox.showinfo("成功","生成完毕")
+        except OSError as a:
+            tkinter.messagebox.showinfo("err","文件错误信息:\n{0}\n{1}".format(a.strerror,a.filename))
+        except Exception as a:
+            tkinter.messagebox.showinfo("err","错误信息:\n{0}".format(a.args))
+    go=tkinter.Button(win,text="  go  ",command=goBack)
+    go.pack()
+
+    
+    # xLabel = tkinter.Label(master=win, text="KAHN Hello world")
+    # xLabel.focus_set()
+    # xLabel.pack()
+    # def xFunc1(event):
+    #     print(f"事件触发键盘输入:{event.char},对应的ASCII码:{event.keycode}")
+    # xLabel.bind("<Key>", xFunc1)
+    
+    win.mainloop()   # #窗口持久化
+
 if __name__=='__main__':
     #main()
-    __test_xml()
+    win()
+    
     pass

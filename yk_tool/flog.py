10e925fea333bcbd0c5d297ff224fb3f14a4d6b5
zheng-dev <zheng6655@163.com>
2024-07-30 13:33:44 +0800

    fixed 进度

--- yk_tool/flog.py
+++ yk_tool/flog.py
@@ -53,10 +53,9 @@ class Find:
                         i2=line.find(endStr,i)
                         if i2>-1:
                             txt=line[i:i2+1]    
-                            self.retList.append((-len(txt),txt,f,lineNum))
+                            self.retList.append((len(txt),txt,f,lineNum))
                     pro.progress_no_sum()        
         del pro
-        print(f"clear:{time.time()}")
         #显示
         try:
             self.retList.sort()
@@ -65,22 +64,6 @@ class Find:
         print(f"sort:{time.time()}")
         #print(self.retList) 
         self.display() if self.calc_page() else print("no match")
-    def __sort_format(self,str1:str):
-        int1=0
-        i=0
-        #TODO 小数、科学计数
-        for char in str1:
-            n=ord(char)
-            isNum=n>=48 and n<=57
-            if isNum and int1==0:
-                int1=i
-                i+=1
-            elif (not isNum) and int1!=0:
-                return int(str1[int1:i])
-            else:
-                i+=1
-        return str1    
-
 
     #计算页码       
     def calc_page(self):
@@ -220,30 +203,37 @@ def clear_data()->tuple[int, int, dict]:
             condLineNum+=1
     return (lineNum,condLineNum,kvList)         
 
-##进度-有总量的
-def Progress(max:int,currIndex:int):
-    #求10分之N
-    rate=100
-    curr=round(currIndex/max*rate)
-    print("\r"+"#"*curr+"_"*(rate-curr),end="")
-
 class Progress:
     currIndex=0
+    max=0
+    __rate=0
+    __sTime=0
+    __eTime=0
+
+    def __init__(self,max1:int=None,rate=10000) -> None:
+        import time
+        self.__rate=rate
+        self.max=max1
+        self.__sTime=time.time()
+        pass
+
+    ##进度-有总量的
+    def Progress(self):
+        curr=round(self.currIndex/self.max*self.__rate)
+        print("\r"+"#"*curr+"_"*(self.__rate-curr),end="")
     ##无总量的,每100数刷一下
     def progress_no_sum(self,rate:int=10000):
         self.currIndex+=1
         if (self.currIndex % rate)==0:
             f='/' if ((self.currIndex)/rate %2)==0 else '\\'    
             print("\r\033[1;32m curr:{0}  {1}  \033[m ".format(self.currIndex,f),end="")
-
-    def end(self):
-        #bug 这里只能del局部变量self,不是del自己
-        del self        
     ##进度完成
     def __del__(self):
+        import time
+        __eTime=time.time()
     #前景：黑色30紅色31綠色32黃色33藍色34紫紅色35青藍色36白色37
     #背景：在前景值 基础上+10
-        print(f"\033[1;37m \n =done={self.currIndex}")
+        print(f"\033[1;37m \n =done={self.currIndex} use {int(__eTime)-int(self.__sTime)} second")
 
 def test():
     print("\033[20A\033[?25l",end="")

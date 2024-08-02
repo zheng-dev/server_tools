64dea23bfe6f63cdb54d5085fbfd199e9c45ebc4
zheng-dev <zheng6655@163.com>
2024-08-02 16:58:47 +0800

    gui找include

--- yk_tool/find_hrl_dir.py
+++ yk_tool/find_hrl_dir.py
@@ -2,34 +2,33 @@
 # coding=utf-8
 #py3.11
 # 运行方式：python f.py ../
-import os,shutil
-
-import sys
+import os,shutil,sys
 
 GAME='game.iml'#文件
 
-def main():
-   if len(sys.argv)>1:
-       dir=sys.argv[1]
-   else:
-       dir="."
-   FindHrlDir.go(dir)
+# def main():
+#    if len(sys.argv)>1:
+#        dir=sys.argv[1]
+#    else:
+#        dir="."
+#    FindHrlDir.go(dir)
 
-   return
+#    return
 
 class FindHrlDir:
     _GFindRet=[]#递归遍历目录时存path用
     def go(dir):
+        os.chdir(dir)
         a=FindHrlDir()
         os.chdir('plugin')
-        a._find_dir("",dir)
+        a._find_dir("","./")
         os.chdir('../')
         a._after_find()
 
     def __init__(self) -> None:
         #检查目录是否是idea项目
         if not os.path.isfile(GAME):
-            raise "pro_dir_err"
+            raise Exception("pro_dir_err")
         pass
     #查找出头目录到全局变量中
     def _find_dir(self,Prent:str,path:str)->None:
@@ -73,31 +72,71 @@ def __test_xml():
     test.attrib['bb']='c'
     tree.write('aa.iml',"UTF-8",True,None,'xml')
 
+class AppCfg:
+    last_dir=""
+    data={}
+
+    def go():
+        import pathlib
+        print(pathlib.Path.home())
+        a=AppCfg()
+        return a
+    def serialize(self):  
+         # 使用instance的__dict__属性来获取实例的所有属性  
+         return {key: value for key, value in self.__dict__.items()}  
+    def __get_cfg(self):
+        import pathlib
+        return str(pathlib.Path.home())+'/.find_hrl_dir.cfg'
+    
+    def __init__(self) -> None:
+        import json
+        with open(self.__get_cfg(),'r+',-1,'utf-8-sig') as f:
+            try:
+                r=json.load(f)
+                self.last_dir=r['last_dir']
+            except:
+                self.last_dir=os.path.dirname(__file__)    
+        pass
+    def save(self):
+        import json
+        with open(self.__get_cfg(),'w+',-1,'utf-8-sig') as f:
+            data=self.serialize()#{'last_dir':self.last_dir}
+            json.dump(data,f)
+    def __del__(self):
+        self.save()
+        pass
+
 def win():
     import tkinter,tkinter.messagebox
     win = tkinter.Tk()
     win.title("iml-hrl目录增加")    # #窗口标题
     win.geometry("300x90+900+110")   # #窗口位置500后面是字母x
 
+    
+
     l1=tkinter.Label(win,text='项目路径')
     l1.pack()
-    dir=tkinter.Entry(win,width=20)
+    dir=tkinter.Entry(win,width=40)
     
-    dir.insert(0,os.path.dirname(__file__))
+    dir.cfg=AppCfg.go()
+    dir.insert(0,dir.cfg.last_dir)
     dir.pack()
     dir.focus_set()
     def goBack():
         pathStr=dir.get()
         print(f"=={pathStr}")
+        global cfg
         try:
-            os.chdir(pathStr)
-            __test_xml()
-            
+            #__test_xml()
+            dir.cfg.last_dir=pathStr
+            FindHrlDir.go(pathStr)
+            dir.cfg.save()
             tkinter.messagebox.showinfo("成功","生成完毕")
         except OSError as a:
             tkinter.messagebox.showinfo("err","文件错误信息:\n{0}\n{1}".format(a.strerror,a.filename))
         except Exception as a:
             tkinter.messagebox.showinfo("err","错误信息:\n{0}".format(a.args))
+                    
     go=tkinter.Button(win,text="  go  ",command=goBack)
     go.pack()
 

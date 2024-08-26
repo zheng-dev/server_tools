65532ad101bca79541024794e9678cac8637c818
zheng-dev <zheng6655@163.com>
2024-08-26 11:21:03 +0800

    server参数

--- yk_tool/pyweb/run.py
+++ yk_tool/pyweb/run.py
@@ -4,8 +4,8 @@ import logging
 
 if __name__=='__main__':
     # 配置日志记录
-    logging.basicConfig(filename='waitress.log', level=logging.INFO)
+    logging.basicConfig(filename='waitress.log', level=logging.ERROR)
     app1=app.create_app()
     app1.static_folder='../static/'
     # app1.run(port=80)
-    serve(app1,port=80,threads=4)
+    serve(app1,port=80,threads=4,host='192.168.22.9',ident='nginx 1.0.0',log_socket_errors=True)

955088bc2f45982c594c643a90674c05a879c705
zheng-dev <zheng6655@163.com>
2024-09-09 14:03:42 +0800

    加多进程测试

--- yk_tool/jira/test.py
+++ yk_tool/jira/test.py
@@ -1,71 +1,95 @@
 #
 # coding=utf-8
-#py3.12.5
-#Auther:zhengzhichun [zheng6655@163.com]
-#Date: 2024-09-06
-#decription:测试协程
+# py3.12.5
+# Auther:zhengzhichun [zheng6655@163.com]
+# Date: 2024-09-06
+# decription:测试协程
 
-import asyncio,time
+import asyncio, time
 from urllib.request import urlopen
 from types import FunctionType
+from multiprocessing import Pool
+
 
 # 计时
-def calc_time(fun1:FunctionType):
-    def wrap(*args,**kargs):
-        s1=time.time()
-        ret=fun1(*args,**kargs)
-        s2=time.time()
-        print(f'{fun1.__name__} use {s2-s1}')
+def calc_time(fun1: FunctionType):
+    def wrap(*args, **kargs):
+        s1 = time.time()
+        ret = fun1(*args, **kargs)
+        s2 = time.time()
+        print(f"{fun1.__name__},timeUse {s2-s1}")
         return ret
+
     return wrap
-    
+
 
 @calc_time
-def get(url:str)->str:
-    try:        
-        # 同步函数不是让出线程,会阻塞.此处只有换专门的异步io
-        r=urlopen(url,timeout=5.0)
+def get_url(url: str) -> str:
+    try:
+        # 同步函数不会让出线程,会阻塞.此处只有换专门的异步io函数,以具有让出线程执行的特性
+        r = urlopen(url, timeout=5.0)
     except:
-        r="0"    
+        r = "0"
     return r
+
 
 async def test(i):
-    print(f'{i}------b')
-    r=get(i)
-    print(f'{i}------e')
+    print(f"{i}------b")
+    r = get_url(i)
+    print(f"{i}------e")
     return r
 
 
-async def main():
-    u='http://192.168.2.9'
+async def crtns_main():
+    # coroutines
+    u = "http://192.168.2.9"
     # 同步io函数在多个任务中也会把线程阻塞，所以两个任务的总用时是累长成了双倍
-    t1=asyncio.create_task(test(u))
-    t2=asyncio.create_task(test(u))
-    l=await asyncio.gather(t1,t2)
-    #await t1
-    #await t2
-    #done,pending=await asyncio.wait({test(u),test(u)},return_when=asyncio.ALL_COMPLETED)
-    #print('main=',done,pending)
-    
+    t1 = asyncio.create_task(test(u))
+    t2 = asyncio.create_task(test(u))
+    l = await asyncio.gather(t1, t2)
+    # await t1
+    # await t2
+    # done,pending=await asyncio.wait({test(u),test(u)},return_when=asyncio.ALL_COMPLETED)
+    # print('main=',done,pending)
 
-@calc_time
-def test_main():
-    
-    print('test main')
-    asyncio.run(main())
-    #thread_test()
-    print('test main====')
-    pass
 
 def thread_test():
+    # 线程
     import threading
+
     # 多线程会被时间片切碎，两个的总用时是重叠的，不会累长
-    t1=threading.Thread(target=test,args=('http://2.2.2.2',))
-    t2=threading.Thread(target=test,args=('http://2.2.2.3',))
+    t1 = threading.Thread(target=test, args=("http://2.2.2.2",))
+    t2 = threading.Thread(target=test, args=("http://2.2.2.3",))
     t1.start()
     t2.start()
     t1.join()
     t2.join()
 
-if __name__=='__main__':
+
+def f(x):
+    import os
+
+    get_url("http://2.2.2.2")
+    print(os.getpid())
+    return x * x
+
+
+def process_test():
+    # 多进程
+    with Pool(5) as p:
+        print(p.map(f, [1, 2, 3, 4, 5, 6]))
+        # print(p.map(f,[1,2,3,4,5,6]))
+
+
+@calc_time
+def test_main():
+    print("test main")
+    process_test()
+    # asyncio.run(crtns_main())
+    # thread_test()
+    print("test main====")
+    pass
+
+
+if __name__ == "__main__":
     test_main()
